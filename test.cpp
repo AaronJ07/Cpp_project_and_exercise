9a6af7c41028b1cdf2903ce039d6c47f5f3ab524
Zhi Hong <j76541512@gmail.com>
2025-12-11 23:01:22 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 
-// We use 'void' because we are modifying the array directly, not returning a number.
 void bubble_sort(int a[], int n)
 {
     for(int i = 0; i < n - 1; i++) 
@@ -9,13 +8,11 @@ void bubble_sort(int a[], int n)
         // Inner loop: moves through the array
         for(int j = 0; j < n - i - 1; j++) 
         {
-            // 1. COMPARE THE VALUE (a[j]), not the index (j)
             if(a[j] > a[j+1]) 
             {
-                // 2. SWAP THE VALUE
-                int temp = a[j];    // Store value of current bucket
-                a[j] = a[j+1];      // Pour next bucket into current
-                a[j+1] = temp;      // Pour stored value into next
+                int temp = a[j];    
+                a[j] = a[j+1];      
+                a[j+1] = temp;      
             }
         }
     }
@@ -26,10 +23,8 @@ int main()
     int n = 6;
     int a[] = {2 , 5 , 1 , 6 , 9 , 3};
 
-    // 1. Perform the sort
     bubble_sort(a, n);
 
-    // 2. Print the array manually (C++ cannot print a whole array at once)
     cout << "The array after sort: ";
     for(int i = 0; i < n; i++) {
         cout << a[i] << " ";
